151204a218a88be264ba1671abde8016b3be2e0d
Dan-Popescu <popescu_dan@outlook.com>
2025-11-08 09:15:27 +0100

    Fix typescript warnings

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,20 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+
+
 declare module '*.svg?component' {
   import type { FunctionalComponent } from 'preact';
   const Component: FunctionalComponent<any>;
   export default Component;
+}
+
+declare module '*.png' {
+  const metadata: import ('astro').ImageMetadata;
+  export default metadata;
+}
+
+declare module '*.webp' {
+  const metadata: import ('astro').ImageMetadata;
+  export default metadata;
 }
