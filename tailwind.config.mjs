2ead60fc523ffebb794a398b4ca6de504a0f18ae
AkamediasTech <technique@akamedias.fr>
2025-11-10 14:06:40 +0100

    Update horizontal spacing layout

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,22 @@
 // tailwind.config.mjs
-import defaultTheme from 'tailwindcss/defaultTheme';
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx}',
-    './public/**/*.html',
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Poppins', ...defaultTheme.fontFamily.sans],
-      },
-      colors: {
-        'brand-blue': '#1264c1',
-        'bright-blue': '#0a7cff',
-      }
+    content: ["./src/**/*.{astro,html,js,jsx,ts,tsx}", "./public/**/*.html"],
+    theme: {
+        extend: {
+            screens: {
+                xs: "400px",
+            },
+            fontFamily: {
+                sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                "brand-blue": "#1264c1",
+                "bright-blue": "#0a7cff",
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
